0123f0d917357c264b41611c62061ec5ee759d27
Manoela Cirilo <manoelacirilo04@gmail.com>
2022-01-24 21:54:15 -0300

    add type hinting

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from src.user import User
 
-client = User.sign_up(name='Kida', email='kida123@gmail.com', password='milo')
-# User.sign_up(name='Kida', email='kida123@gmail.com', password='')
 
-client.login(email='kida123@gmail.com', password='milo')
-# client.login(email='123@gmail.com', password='mi')
+def main() -> None:
+    client = User.sign_up(name='Kida', email='kida123@gmail.com', password='milo')
+    # User.sign_up(name='Kida', email='kida123@gmail.com', password='')
+
+    client.login(email='kida123@gmail.com', password='milo')
+    # client.login(email='123@gmail.com', password='mi')
+
+
+if __name__ == '__main__':
+    main()
--- src/user.py
+++ src/user.py
@@ -1,41 +1,48 @@
+from typing import Optional
+
 import bcrypt
 
 
 class User:
-    counter = 0
+    counter: int = 0
 
-    def __init__(self, name, email, password):
+    def __init__(self, name: str, email: str, password: str) -> None:
         self.__id = User.counter + 1
-        self.__name = name
-        self.__email = email
-        self.__password = User.__encrypt_password(password)
-        self.__logged = False
+        self.__name: str = name
+        self.__email: str = email
+        self.__password: bytes = User.__encrypt_password(password)
+        self.__logged: bool = False
         User.counter = self.__id
 
     @staticmethod
-    def sign_up(name, email, password):
+    def sign_up(name: str, email: str, password: str) -> Optional['User']:
         if name == '' or email == '' or password == '':
             print('Registration not done')
             return
 
-        client = User(name, email, password)
+        client: User = User(name, email, password)
         print(f'User {client.__name} successfully registered.')
         return client
 
     @staticmethod
-    def __encrypt_password(password):
-        salt = bcrypt.gensalt()
+    def __encrypt_password(password: str) -> bytes:
+        salt: bytes = bcrypt.gensalt()
         return bcrypt.hashpw(password.encode('utf8'), salt)
 
-    def login(self, email, password):
+    def login(self, email: str, password: str) -> None:
         if email == self.__email and self.check_password(password):
-            self.__logged = True
+            self.logged = True
             print(f'Welcome {self.__name}')
         else:
             print('Invalid credentials')
 
-    def check_password(self, password_to_check):
+    def check_password(self, password_to_check: str) -> bool:
         return bcrypt.checkpw(password_to_check.encode('utf8'), self.__password)
 
-    def is_logged(self):
+    @property
+    def logged(self) -> bool:
         return self.__logged
+
+    @logged.setter
+    def logged(self, new_logged: bool) -> None:
+        self.__logged = new_logged
