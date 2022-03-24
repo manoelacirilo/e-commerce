62285c63ea989ca0a196a317954890d1e428c225
Manoela Cirilo <manoelacirilo04@gmail.com>
2022-03-23 21:10:19 -0300

    create test to add to existing cart

--- e_commerce/service/cart_service.py
+++ e_commerce/service/cart_service.py
@@ -3,8 +3,12 @@ from e_commerce.repository import cart_repository, item_repository
 
 class CartService:
     @staticmethod
-    def add_to_cart(product_id, quantity, user):
-        cart = cart_repository.add(user_id=user.id)
+    def add_to_cart(product_id, quantity, user_id):
+        cart = cart_repository.get_by_user_id(user_id)
+
+        if not cart:
+            cart = cart_repository.add(user_id=user_id)
+
         item_repository.add(quantity=quantity, product_id=product_id, cart_id=cart.id)
 
         return cart
